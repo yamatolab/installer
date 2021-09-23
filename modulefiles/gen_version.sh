cfb5cc771863dbfb2f35c61c889feb7be0795d00
Wataru Sugiura <flat35hd99@gmail.com>
2021-09-23 20:19:05 +0900

    Fix

--- modulefiles/gen_version.sh
+++ modulefiles/gen_version.sh
@@ -3,4 +3,4 @@
 pkg_name=$1
 version=$2
 
-sed "s/___VERSION/$version/g" templates/$pkg_name > $PACKAGE_DIR_PREFIX/$pkg_name/$version
+sed "s/___VERSION/$version/g" templates/$pkg_name > $MODULEFILES_PREFIX/$pkg_name/$version
